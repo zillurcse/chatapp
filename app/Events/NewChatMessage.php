2233344bd679a01b23eb83bf9cca9a87f258ceaa
zillurrahmannoyan <zillrrahman68582@gmail.com>
2021-05-08 01:01:01 +0600

    Chat App

--- app/Events/NewChatMessage.php
+++ app/Events/NewChatMessage.php
@@ -10,10 +10,11 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use App\Models\ChatMessage;
+
 class NewChatMessage implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
-    public $chatMessage;
+    private $chatMessage;
 
     /**
      * Create a new event instance.
